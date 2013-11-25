dd0a6f718960929617867a93b47d7ed5850ea540
noone <noone>
2013-11-24 18:39:21 -0700

    Added openbox stuffs

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,9 @@
+
+/usr/bin/tint2 &
+/usr/bin/nitrogen --restore &
+/usr/bin/nm-applet &
+/usr/bin/bluetooth-applet &
+/usr/bin/gnome-keyring-daemon --daemonize --start
+
+#/usr/bin/udiskie &
+/usr/bin/conky &
