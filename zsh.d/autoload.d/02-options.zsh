ae0ce7e456c631bf6d4f3d4a849f875c74af05f2
npotts <npotts@users.noreply.github.com>
2018-01-02 14:12:05 -0700

    Enable autocomplete

--- zsh.d/autoload.d/02-options.zsh
+++ zsh.d/autoload.d/02-options.zsh
@@ -8,3 +8,8 @@ setopt auto_cd
 setopt globdots
 
 
+
+autoload -U compinit
+compinit
+zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
+zstyle ':completion:*:warnings' format '%B no matches for: %d%b'
