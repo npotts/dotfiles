eb941861befec78742dddcb246c403d82d27024e
npotts <npotts@users.noreply.github.com>
2019-03-14 07:13:50 -0600

    renamed file

--- zsh.d/autoload.d/03-pyenv.zsh
+++ zsh.d/autoload.d/03-pyenv.zsh
@@ -2,4 +2,5 @@
 # In order to build pyenv I needed tose  the CPPFLAGS like thus on macOS
 # CPPFLAGS=-I/usr/local/opt/zlib/include -I/usr/local/opt/sqlite3/include pyenv install -v 3.6.8
 export PYENV_ROOT="$HOME/.pyenv"
+s
 eval "$(pyenv init -)"
