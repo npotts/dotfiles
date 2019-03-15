1f6dc4d8896683ca52c01571deefd5cc8d781620
npotts <npotts@users.noreply.github.com>
2019-03-15 13:41:05 -0600

    More lazy rubyism

---
+++ zsh.d/autoload.d/80-ruby.zsh
@@ -0,0 +1,24 @@
+
+if [ -e /usr/local/opt/chruby/share/chruby/chruby.sh ]; then 
+  source /usr/local/opt/chruby/share/chruby/chruby.sh
+fi 
+
+if [ -e /usr/local/opt/chruby/share/chruby/auto.sh ]; then
+  source /usr/local/opt/chruby/share/chruby/auto.sh
+fi
+
+
+
+
+function ruby_rbenv_install() {
+  # https://stackoverflow.com/questions/9602806/bundler-not-working-with-rbenv-could-not-find-gem
+  rm -fr  ~/.rbenv ~/.gem
+  rbenv install $1
+  rbenv rehash # for paranoia's sake
+  rbenv global $1
+  rbenv local $1
+  gem env # to check that everything was pointing at 1.9 and not the system
+  gem install --no-ri --no-rdoc bundler
+  rbenv rehash # necessary to load up the bundle executable
+  bundle --path=vendor/bundle
+}
