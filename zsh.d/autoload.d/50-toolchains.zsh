b2e024dfe98be7eb84a742d80d380e0cd8398dba
npotts <npotts@users.noreply.github.com>
2019-03-28 17:48:44 -0400

    on macOS wotj homebrew ssl, modifiy env to compile properly

--- zsh.d/autoload.d/50-toolchains.zsh
+++ zsh.d/autoload.d/50-toolchains.zsh
@@ -6,3 +6,11 @@ if [ -d "/opt/msp430" ]; then
   #Add to paths
   export MSP430ROOT="/opt/msp430/"
 fi
+
+
+
+if [ -d "/usr/local/opt/openssl" ]; then
+  #brew's openssl install alter CPPFlags and LDFlags
+  export CPPFLAGS=-I/usr/local/opt/openssl/include
+  export LDFLAGS=-L/usr/local/opt/openssl/lib
+fi
